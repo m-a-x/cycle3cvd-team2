67ed256b19a8de472c860aad9db838668c403e07
Yanyu Zheng <yz2690@columbia.edu>
2016-03-06 23:32:58 -0500

    Crop all heads from pics with xml info

--- lib/getFace.R
+++ lib/getFace.R
@@ -1,8 +1,19 @@
 getFace = function(name,xmlDir,picDir){
+  print(name)
   xml = paste(paste(xmlDir,name,sep = "/"), "xml", sep = ".")
   pic = paste(paste(picDir,name,sep = "/"), "jpg", sep = ".")
-  ann = xmlParse(xml)
-  head_box = xmlToDataFrame(ann, nodes=getNodeSet(ann,"//annotation/object/bndbox"))
-  img = readImage(pic)
-  head = img[head_box['xmin']:head_box['xmax'],head_box['ymin'],head_box['ymax']]
+  ann <- xmlParse(xml)
+  head_box <- xmlToDataFrame(ann, nodes=getNodeSet(ann,"//annotation/object/bndbox"))
+  head_box = head_box %>%
+    lapply(toNumeric) %>%
+    unlist()
+  img = pic %>%
+    readImage() %>%
+    channel(mode = 'gray') %>%
+    imageData()
+  #print(dim(img))
+  #print(head_box)
+  head = img[head_box[1]:head_box[3], head_box[2]:head_box[4]]
 }
+
+toNumeric = function(f){as.numeric(levels(f))[f]}
