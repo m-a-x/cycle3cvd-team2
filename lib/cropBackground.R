6a90ce1b78e30f4c9992bdea4df3d20c2eecb505
Yanyu Zheng <yz2690@columbia.edu>
2016-03-11 02:09:01 -0500

    Debugging cropBackground.R

--- lib/cropBackground.R
+++ lib/cropBackground.R
@@ -1,12 +1,32 @@
 cropBackground = function(imageData,step,thresholdSize,thresholdPValue) {
-  listOut = list(imageData[1:step,],imageData[,(ncol(imageData)-step+1):ncol(imageData)],imageData[(nrow(imageData)-step+1):nrow(imageData),],imageData[,1:step])
+  listOut = list(imageData[1:step,],imageData[(nrow(imageData)-step+1):nrow(imageData),],imageData[,1:step],imageData[,(ncol(imageData)-step+1):ncol(imageData)])
   listOut = lapply(listOut,as.vector)
-  R = c(6,nrow(imageData)-5)
-  C = c(6,ncol(imageData)-5)
-  while(min(range(Row),range(Column)) > thresholdSize && max(PValue) > thresholdPValue){
-    listIn = list(imageData[R[1]:(R[1]+step-1),],imageData[,(C[2]-step+1):C[2]],imageData[(R[2]-step+1):R[2],],imageData[,C[1]:(C[1]+step-1)])
-    listIn = lapply(listIn,as.vector)
-    test = mapply(t.test, listOut, listIn)
-    PValue = test['p.value',]
+  Ind = c(1+step, nrow(imageData)-step, 1+step, ncol(imageData)-step)
+  listIn = list(imageData[Ind[1]:(Ind[1]+step-1),],imageData[(Ind[2]-step+1):Ind[2],],imageData[,Ind[3]:(Ind[3]+step-1)],imageData[,(Ind[4]-step+1):Ind[4]])
+  listIn = lapply(listIn,as.vector)
+  test = mapply(t.test, listOut, listIn,paired = TRUE)
+  PValue = do.call(c,test['p.value',])
+  while(Ind[2]-Ind[1] > thresholdSize && Ind[4]-Ind[3] > thresholdSize && max(na.omit(PValue)) > thresholdPValue){
+    print("p")
+    update = which.max(PValue)
+    Ind[update] = Ind[update]-(-1)^update*step
+    if(update == 1){
+      listOut[[1]] = listIn[[1]]
+      listIn[[1]] = imageData[Ind[1]:(Ind[1]+step-1),]
+    }else(update == 2){
+      listOut[[2]] = listIn[[2]]
+      listIn[[2]] = imageData[(Ind[2]-step+1):Ind[2],]
+    }else(update == 3){
+      listOut[[3]] = listIn[[3]]
+      listIn[[3]] = imageData[,Ind[3]:(Ind[3]+step-1)]
+    }else(update == 4){
+      listOut[[4]] = listIn[[4]]
+      listIn[[4]] = imageData[,(Ind[4]-step+1):Ind[4]]
+    }
+    test = t.test(lisOut[[update]],listIn[[update]],paired = TRUE)
+    PValue[update] = test.PValue
+    print(test)
+    print(PValue)
   }
+  return(imageData[Ind[1]:Ind[2],Ind[3]:Ind[4]])
 }
